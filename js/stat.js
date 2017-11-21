1e5c2ab046aceff9afd7abe25c52545b9ed6f1d1
Gaspar Color <acl200@yandex.ru>
2017-11-22 02:40:30 +0500

    fixes

--- js/stat.js
+++ js/stat.js
@@ -10,9 +10,6 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillRect(100, 10, 420, 270);
 
 
-
-
-
   ctx.fillStyle = '#000'; // black;
   ctx.font = '16px PT Mono';
 
@@ -31,33 +28,33 @@ window.renderStatistics = function (ctx, names, times) {
   }
 
   var histogramWidth = 40;
-  var barHeigth = 150;
-  var step = barHeigth / (max - 0); // px;
+  var histogramHeigth = 150;
+  var step = histogramHeigth / (max - 0); // px;
 
   ctx.fillText('Худшее время: ' + max.toFixed(2) + 'мс у игрока ' + names[maxIndex], 120, 60);
 
-   // px;
+  // px;
   var indent = 70;    // px;
   var initialX = 140; // px;
   var initialY = 80;  // px;
   var lineHeight = 60;//
 
 
-  for(var i = 0; i < times.length; i++) {
+  for (var i = 0; i < times.length; i++) {
 
     //fillRect(x, y, width, height)
     ctx.fillStyle = 'black';
-    ctx.fillText(Math.round(times[i]),  initialX - 10  + indent * i, 90);
-    if(names[i] === 'Вы'){
+    ctx.fillText(Math.round(times[i]), initialX  + indent * i, 90);
+    if (names[i] === 'Вы') {
       ctx.fillStyle = "rgba(255, 0, 0, 1)";
-    }else{
+    } else {
       ctx.fillStyle = "rgba(0, 0, 255, 0.3)";
       ctx.fillStyle = 'rgba(0, 0, 255,' + Math.random().toFixed(1) + ')';
     }
-    ctx.fillRect(initialX  + indent * i, 100, histogramWidth, times[i] * step );
+    ctx.fillRect(initialX + indent * i, 100, histogramWidth, times[i] * step);
     ctx.fillStyle = 'black';
-    //CanvasRenderingContext2D.fillText(text, x, y [, maxWidth]);
-    ctx.fillText(names[i],  initialY + lineHeight + indent * i,270);
+    //fillText(text, x, y [, maxWidth]);
+    ctx.fillText(names[i], initialY + lineHeight + indent * i, 270);
 
   }
 };
