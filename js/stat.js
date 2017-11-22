36a7b4bec3ecd4cf914b603bd31954796a99ea85
Alexander Kartashov <a.kartashov@edgex.ru>
2017-11-22 18:27:15 +0500

    fixes

--- js/stat.js
+++ js/stat.js
@@ -4,6 +4,15 @@
 
 window.renderStatistics = function (ctx, names, times) {
 
+  var HISTOGRAM_HEIGHT = -150;
+  var HISTOGRAM_WIDTH = 40;
+  var INITIAL_X = 160;
+  var COLUMN_WIDTH = 80;
+
+  var getRandomValue = function (minValue, maxValue) {
+    return Math.random() * (maxValue - minValue) + minValue;
+  };
+
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
   ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
@@ -18,44 +27,30 @@ window.renderStatistics = function (ctx, names, times) {
 
 
   var max = -1;
-  var maxIndex = -1;
 
   for (var i = 0; i < times.length; i++) {
     var time = times[i];
     if (time > max) {
       max = time;
-      maxIndex = i;
     }
   }
 
-  var getRandomValue = function (minValue, maxValue) {
-    return Math.random() * (maxValue - minValue) + minValue;
-  };
-
-  var histogramHeigth = -150;
-  var step = histogramHeigth / (max - 0);
+  var step = HISTOGRAM_HEIGHT / (max - 0);
 
   ctx.fillText('Список результатов: ', 120, 60);
 
-  var histogramWidth = 40;
-  var initialX = 160;
-  var columnWidth = 80;
-
-
   for (i = 0; i < times.length; i++) {
-
     ctx.fillStyle = 'black';
 
     // Скорость прохождения fillText(text, x, y [, maxWidth]);
-    ctx.fillText(Math.round(times[i]), initialX + columnWidth * i, 90);
+    ctx.fillText(Math.round(times[i]), INITIAL_X + COLUMN_WIDTH * i, 90);
     ctx.fillStyle = (names[i] === 'Вы') ? 'red' : 'rgba(0, 0, 255, ' + getRandomValue(0.1, 1) + ')';
 
     // Гистограмма fillRect(x, y, width, height)
-    ctx.fillRect(initialX + columnWidth * i, 250, histogramWidth, times[i] * step);
+    ctx.fillRect(INITIAL_X + COLUMN_WIDTH * i, 250, HISTOGRAM_WIDTH, times[i] * step);
     ctx.fillStyle = 'black';
 
     // Имя игрока - fillText(text, x, y [, maxWidth]);
-    ctx.fillText(names[i], initialX + columnWidth * i, 265);
-
+    ctx.fillText(names[i], INITIAL_X + COLUMN_WIDTH * i, 265);
   }
 };
